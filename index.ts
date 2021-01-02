c69fb14bf65c2423d8bbc78d6918dfa71e0d0f5e
zhijiec99 <zhijiec99@hotmail.com>
2021-01-01 20:20:03 -0500

    Add function to quickly retrieve 3000 series NVidia cards

--- index.ts
+++ index.ts
@@ -1,4 +1,13 @@
 import {request} from './src/Util/NetworkUtil';
 import cheerio from 'cheerio';
-import { NewEggLookup } from './src/Handlers/Newegg';
+import { NewEggLookup , get3000SeriesProductIDsFromSearchQuery} from './src/Handlers/Newegg';
 
+
+async function test(q: string){
+    let s = await get3000SeriesProductIDsFromSearchQuery(q);
+    console.log(s);
+}
+
+test("rtx 3060");
+test("rtx 3070");
+test("rtx 3080");
--- src/Handlers/Newegg.ts
+++ src/Handlers/Newegg.ts
@@ -27,3 +27,30 @@ function checkStock($: cheerio.Root, url: string){
     let isSoldOut = soldOutText == "Sold Out"
     Logger.logStockStatus("newegg", product.substr(1, 50), url, isSoldOut);
 }
+
+export async function get3000SeriesProductIDsFromSearchQuery(query: string): Promise<string[]> {
+    let url = `https://www.newegg.ca/p/pl?d=${encodeURI(query)}&N=601357282%20100007708&PageSize=96`;
+    try {
+        let response = await request(url);
+        if(!response.data){
+            Logger.err(`No data while searching for ${query}`);
+            return [];
+        }
+
+        const root = cheerio.load(response.data);
+        return parseProductIDs(root);
+    } catch (err){
+        Logger.err(`Fetch for ${url} went wrong`)
+        return [];
+    }
+}
+
+function parseProductIDs($: cheerio.Root): string[] {
+    let productIDs: string[] = [];
+    $(".item-features li:nth-child(6)").each((i, el) => {
+        let s = $(el).text().split(" ")[2];
+        productIDs.push(s);
+    });
+
+    return productIDs;
+}
