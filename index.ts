4fe400c1e43b88d9efb95f5bbc2a2bac1526721b
zhijiec99 <zhijiec99@hotmail.com>
2021-01-03 11:31:13 -0500

    Add service calling

--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import {request} from './src/Util/NetworkUtil';
 import cheerio from 'cheerio';
 import { NewEggLookup , get3000SeriesProductIDsFromSearchQuery} from './src/Handlers/Newegg';
+import { NewEggStockService } from './src/NewEggStockService';
 
 
-async function test(q: string){
+async function getProductIDs(q: string){
     let s = await get3000SeriesProductIDsFromSearchQuery(q);
     console.log(s);
 }
 
-test("rtx 3060");
-test("rtx 3070");
-test("rtx 3080");
+let service = new NewEggStockService();
+service.init(60*1000)
+// getProductIDs("rtx 3060");
+// getProductIDs("rtx 3070");
+// getProductIDs("rtx 3080");
+// N82E16814487530
+// NewEggLookup("N82E16814932377");
